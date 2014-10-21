991e7508e573dd1082e176c1ad841a31136d6968
KangChangKi <changki.kang@gmail.com>
2014-10-21 20:53:28 +0900

    added comment

--- custom/01function-keys.el
+++ custom/01function-keys.el
@@ -1,2 +1,11 @@
-(global-set-key [f5] (lambda () (interactive) (indent-region (point-min) (point-max))))
-(global-set-key [f6] 'revert-buffer)
+;;
+;;
+
+(global-set-key [f5] 'revert-buffer)
+(global-set-key [f6] (lambda () (interactive) (indent-region (point-min) (point-max))))
+
+;; at 12projectile.el
+;; (global-set-key [f7] 'projectile-find-file)
+;; (global-set-key [f8] 'projectile-switch-to-buffer)
+;; (global-set-key [f9] 'projectile-grep)
+;; (global-set-key [f10] 'projectile-replace)
